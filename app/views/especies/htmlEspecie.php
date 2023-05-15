411a995785f882bb069372e05de4c7025813fbd9
amandapscheer <128150812+amandapscheer@users.noreply.github.com>
2023-05-15 09:41:18 -0300

    Update htmlEspecie.php

--- app/views/especies/htmlEspecie.php
+++ app/views/especies/htmlEspecie.php
@@ -61,7 +61,7 @@ Class EspecieHTML {
             echo "<div class='card-body'>";
             echo "<h5 class='card-title nome-soc'>". $especie->getNomePopular() ."</h5>";
             echo "<p class='card-text nome-texto'>".$frut.$tox.$med.$come.$rara.$exot."</p>";
-            echo "<a href='editarEspecie.php?id=".$especie->getIdEspecie()."' class='btn btn-primary'>Editar</a>";
+            echo "<a href='editarEspecie.php?id=".$especie->getIdEspecie()."' class='btn btn-primary editar'>Editar</a>";
             echo "<a href='deletarEspecie.php?id=".$especie->getIdEspecie()."' onclick='return confirm(\"Confirma a exclusão da Espécie? Todas as plantas associadas a essa espécie também serão excluídas!\");' class='btn btn-alert excluir'>Excluir</a>";
             echo "<br>";
             echo "</div>";
