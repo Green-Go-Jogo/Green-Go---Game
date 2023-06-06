83d7d3144bf1aad284923f2b1ec78186ca9b6d96
amandapscheer <128150812+amandapscheer@users.noreply.github.com>
2023-06-06 00:46:04 -0300

    CSS Arrumado
    CSS arrumadinho das zonas/especies/plantas/equipes

--- app/views/especies/listEspecies.php
+++ app/views/especies/listEspecies.php
@@ -41,22 +41,9 @@ include_once(__DIR__."/htmlEspecie.php");
         text-decoration: none;
     }
 
-        a.editar:hover {
-            color: #ebf0f1 !important;
-            background-color: #04574d; }
-
-            a.editar{
-            color: #ebf0f1 !important;
-            background-color: #338a5f; }
-
-            a.excluir:hover {
-            color: var(--branco);
-            background-color: #f0b6bc !important;
-            border-radius: 5px; }
-
     </style>
 
-    <head>
+<head>
 
 <?php include_once("../../bootstrap/header.php");?>
 <link rel="stylesheet" href="css/index.css">
@@ -75,7 +62,7 @@ include_once(__DIR__."/htmlEspecie.php");
    
     <div style="margin: 40px 10px 0px 10px;">
     <div style="float: right; padding-right: 70px;">
-    <a class="btn incluir" href="adicionarEspecie.php"> 
+    <a class="btn incluir" href="adicionarEspecie.php">
 
     <svg xmlns="http://www.w3.org/2000/svg" width="90" height="60" fill="#04574d" viewBox="0 0 16 16">
     <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z"/> </svg>
--- app/views/zones/htmlZona.php
+++ app/views/zones/htmlZona.php
@@ -1,19 +1,37 @@
 <html>
 <link rel="stylesheet" href="../css/listPlanta.css">
 <style>
+
+    .btn:hover {
+        color: #f58c95;
+        transform: scale(1.05); }
+
         a.editar:hover {
             color: #ebf0f1 !important;
             background-color: #04574d; }
 
-            a.editar{
+        a.editar{
             color: #ebf0f1 !important;
             background-color: #338a5f; }
 
-            a.excluir:hover {
+        a.excluir {
+            color: #f0b6bc;
+            border-color: #f0b6bc;
+        }
+
+        a.excluir:hover {
             color: var(--branco);
             background-color: #f0b6bc !important;
             border-radius: 5px; }
-    </style>
+
+        #NomeZona {
+            background-color: #C05367 !important;
+            color: #FFFFFF !important;
+            border-radius: 20px;
+            transform: scale(1.05);
+        }
+
+</style>
 </html>
 
 <?php
@@ -27,7 +45,7 @@
             echo "<br>";
             echo "<div class='card' style='width: 22rem;'>";
             echo "<div class='card-body'>";
-            echo "<h5 class='card-title nome-soc'>". $zona->getNomeZona() ."</h5>";
+            echo "<h5 class='card-title nome-soc' id='NomeZona'>". $zona->getNomeZona() ."</h5>"."<br>";
             echo "<p class='card-text nome-texto'> Quantidade de Plantas: ".$zona->getQntdPlanta()."<br> Pontos Totais: ".$zona->getPontosTotais()."</p>";
             echo "<a href='editarZona.php?id=".$zona->getIdZona()."' class='btn btn-primary editar'>Editar</a>";
             echo "<a href='deletarZona.php?id=".$zona->getIdZona()."' onclick='return confirm(\"Confirma a exclusão da Zona? Todas as plantas associadas a essa zona também serão excluídas!\");' class='btn btn-alert excluir'>Excluir</a>";
