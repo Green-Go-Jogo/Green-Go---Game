c4b70607d496e75e171c8966c7d3d2875f0873c2
amandapscheer <128150812+amandapscheer@users.noreply.github.com>
2023-07-05 23:44:09 -0300

    Mais mudanças no IndexADM
    Agora não é para estar zuado em 99% dos dispositivos

--- app/views/indexADM.php
+++ app/views/indexADM.php
@@ -28,9 +28,10 @@ else if (!isset($_SESSION['adm']) && !isset($_SESSION['normal'])) {
 
     <style>
 
+
 #titulo {
     font-size: 400%;
-    margin-left: 25.5%;
+    margin-left: 26%;
     text-align: center !important;
     color: #338a5f;
     font-family: Poppins;
@@ -38,91 +39,10 @@ else if (!isset($_SESSION['adm']) && !isset($_SESSION['normal'])) {
 
 #projeto {
     font-size: 340%;
-    margin-left: 17%;
     border-radius: 5px;
     color: #FFFFFF; 
+    margin-left: 43%;
     background-color: #f0b6bc;
-    padding-top: 1%;
-    padding-bottom: 1%;
-    padding-left: 4%;
-    padding-right: 4%;
-    border-style: dashed;
-    border-color: #f58c95;
-    font-family: Poppins-semibold;
-}
-
-#zona {
-    font-size: 340%;
-    margin-left: 1.5%;
-    margin-right: 1.5%;
-    border-radius: 5px;
-    color: #FFFFFF; 
-    background-color: #f0b6bc;
-    padding-top: 1%;
-    padding-bottom: 1%;
-    padding-left: 3.5%;
-    padding-right: 3.5%;
-    border-style: dashed;
-    border-color: #f58c95;
-    font-family: Poppins-semibold;
-}
-
-#especie {
-    font-size: 340%;
-
-    border-radius: 5px;
-    color: #FFFFFF; 
-    background-color: #f0b6bc;
-    padding-top: 1%;
-    padding-bottom: 1%;
-    padding-left: 2.9%;
-    padding-right: 2.9%;
-    border-style: dashed;
-    border-color: #f58c95;
-    font-family: Poppins-semibold;
-}
-
-#planta {
-    font-size: 340%;
-    margin-left: 17%;
-    border-radius: 5px;
-    color: #FFFFFF; 
-    background-color: #f0b6bc;
-    padding-top: 1%;
-    padding-bottom: 1%;
-    padding-left: 3.9%;
-    padding-right: 3%;
-    border-style: dashed;
-    border-color: #f58c95;
-    font-family: Poppins-semibold;
-}
-
-#equipe {
-    font-size: 340%;
-    margin-left: 1.5%;
-    margin-right: 1.5%;
-    border-radius: 5px;
-    color: #FFFFFF; 
-    background-color: #f0b6bc;
-    padding-top: 1%;
-    padding-bottom: 1%;
-    padding-left: 2%;
-    padding-right: 2%;
-    border-style: dashed;
-    border-color: #f58c95;
-    font-family: Poppins-semibold;
-}
-
-#conta {
-    font-size: 340%;
-
-    border-radius: 5px;
-    color: #FFFFFF; 
-    background-color: #f0b6bc;
-    padding-top: 1%;
-    padding-bottom: 1%;
-    padding-left: 6.3%;
-    padding-right: 6.3%;
     border-style: dashed;
     border-color: #f58c95;
     font-family: Poppins-semibold;
@@ -130,7 +50,7 @@ else if (!isset($_SESSION['adm']) && !isset($_SESSION['normal'])) {
 
 #partida {
     font-size: 340%;
-    margin-left: 24%;
+    margin-left: 18%;
     border-radius: 5px;
     color: #FFFFFF; 
     background-color: #04574d;
@@ -149,19 +69,23 @@ else if (!isset($_SESSION['adm']) && !isset($_SESSION['normal'])) {
 
 <br>
 
-<a id="titulo"> PORTAL DO PROFESSOR </a> <br><br><br><br>
+<a id="titulo"> PORTAL DO PROFESSOR </a> <br><br>
 
-<a href="../views/projetoADM.php" id="projeto">Projeto</a>
-<a href="../views/zones/listZonas.php" id="zona">Zonas</a>
-<a href="../views/especies/listEspecies.php" id="especie">Espécies</a> <br><br><br>
-<a href="../views/plantas/listPlantas.php" id="planta">Plantas</a>
-<a href="../views/equipes/listEquipes.php" id="equipe">Equipes</a>
-<a href="../views/users/perfil.php" id="conta">Perfil</a> <br><br><br><br><br>
-<a href="INSERIR CAMINHO DA PARTIDA" id="partida">Cadastrar Partida</a> <br><br><br><br>
+<div class="w-100">
+    
+<ul class="pagination pagination-lg justify-content-center flex-wrap flex-column flex-md-row">
+    <li class="container" onclick="ativar(this)"> <a href="../views/projetoADM.php" id="projeto"> <img src="../views/img/logo.png" style="width: 30px; margin-right: 5px;">Projeto</a></li> <br>
+    <li class="container" onclick="ativar(this)"> <a href="../views/zones/listZonas.php" id="projeto"> <img src="../views/img/logo.png" style="width: 30px; margin-right: 5px;">Zonas</a></li> <br>
+    <li class="container" onclick="ativar(this)"> <a href="../views/especies/listEspecies.php" id="projeto"> <img src="../views/img/logo.png" style="width: 30px; margin-right: 5px;">Espécies</a></li> <br>
+    <li class="container" onclick="ativar(this)"> <a href="../views/plantas/listPlantas.php" id="projeto"> <img src="../views/img/logo.png" style="width: 30px; margin-right: 5px;">Plantas</a></li> <br>
+    <li class="container" onclick="ativar(this)"> <a href="../views/equipes/listEquipes.php" id="projeto"> <img src="../views/img/logo.png" style="width: 30px; margin-right: 5px;">Equipes</a></li> <br>
+    <li class="container" onclick="ativar(this)"> <a href="../views/users/perfil.php" id="projeto"> <img src="../views/img/logo.png" style="width: 30px; margin-right: 5px;">Perfil</a></li>
+</ul> <br><br>
+
+<ul class="pagination pagination-lg justify-content-center flex-wrap flex-column flex-md-row">
+<li class="container" onclick="ativar(this)"> <a href="INSERIR CAMINHO DA PARTIDA" id="partida">Cadastrar Partida</a></li>
+</ul> <br><br>
 
 </body>
 
-<div class="container-fluid" id="rodape">
-            
-            </div>
 </html>
