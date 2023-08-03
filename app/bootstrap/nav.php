c3fcf67cc44a1e988cdde94cd262071e0590bcfe
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-08-02 21:28:58 -0300

    Navs

--- app/bootstrap/nav.php
+++ app/bootstrap/nav.php
@@ -67,7 +67,7 @@
             </div>
         </div>
     </nav>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+ 
 
     <!-- Adicione o link para o Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
