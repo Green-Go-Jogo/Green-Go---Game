d339535c652d97c958c38c582608ac1b0af9edfc
amandapscheer <128150812+amandapscheer@users.noreply.github.com>
2023-09-05 21:30:08 -0300

    Botão Incluir
    Botão para o Nikolas

--- app/views/plantas/htmlPlanta.php
+++ app/views/plantas/htmlPlanta.php
@@ -4,6 +4,12 @@
 
 <style>
 
+#botaomais {
+    justify-content : right;
+    display : flex;
+    transform: scale(1.05);
+}
+
     .btn:hover {
         color: #f58c95;
         transform: scale(1.05); }
@@ -79,11 +85,6 @@
     }
 }
 
-.btn.incluir {
-    justify-content : right;
-    display : flex;
-    transform: scale(1.05);
-}
 
 
 </style>
@@ -92,7 +93,7 @@
 Class PlantaHTML {
     public static function desenhaPlanta($plantas) {
         echo "<div class='container text-center'>";
-        echo "<a class='btn incluir' href='adicionarPlanta.php'>
+        echo "<a class='btn incluir' href='adicionarPlanta.php' id='botaomais'>
         <svg xmlns='http://www.w3.org/2000/svg' width='90' height='60' fill='#20A494' viewBox='0 0 16 16'>
         <path d='M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z'/>
         </svg>
