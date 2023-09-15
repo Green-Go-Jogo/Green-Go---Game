bcdc6f8889fddab8d0e05b30af7f043d259300ce
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-14 22:42:15 -0300

    Create teste.php

---
+++ app/views/partidas/teste.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leitura de QR Code</title>
+</head>
+<body>
+    <h1>Leitura de QR Code</h1>
+    <button id="startButton">Abrir Câmera</button>
+    <video id="video" style="display:none;"></video>
+
+    <script src="../js/camera.js"></script>
+</body>
+</html>
