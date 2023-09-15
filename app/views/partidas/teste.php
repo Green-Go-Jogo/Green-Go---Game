ffa43c522c16477d7cffed3f6f1195d3b9c1b077
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-14 22:51:21 -0300

    Update teste.php

--- app/views/partidas/teste.php
+++ app/views/partidas/teste.php
@@ -1,38 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leitura de QR Code</title>
+    <title>Camera Access</title>
 </head>
 <body>
-    <h1>Leitura de QR Code</h1>
-    <button id="startButton">Abrir Câmera</button>
-    <video id="video" style="display:none;"></video>
+    <button id="startCamera">Abrir Câmera</button>
+    <video id="cameraFeed" autoplay style="display: none;"></video>
 
     <script>
-        // Seleciona o botão e o elemento de vídeo
-        const startButton = document.getElementById('startButton');
-        const video = document.getElementById('video');
-
-        // Verifica se o navegador suporta a API getUserMedia
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-            // Adiciona um evento de clique ao botão
-            startButton.addEventListener('click', function () {
-                // Solicita permissão para acessar a câmera
-                navigator.mediaDevices.getUserMedia({ video: true })
-                    .then(function (stream) {
-                        // Define o vídeo para exibir o feed da câmera
-                        video.srcObject = stream;
-                        video.play();
-                    })
-                    .catch(function (error) {
-                        console.error('Erro ao acessar a câmera: ', error);
-                    });
-            });
-        } else {
-            console.error('O navegador não suporta a API getUserMedia.');
-        }
+        // Event listener para o botão
+        document.getElementById('startCamera').addEventListener('click', function() {
+            // Verifique se o navegador suporta a API WebRTC
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                // Acesso à câmera traseira
+                navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+                .then(function(stream) {
+                    // Exiba o feed da câmera em um elemento de vídeo
+                    var video = document.getElementById('cameraFeed');
+                    video.style.display = 'block';
+                    video.srcObject = stream;
+                })
+                .catch(function(error) {
+                    console.error('Erro ao acessar a câmera:', error);
+                });
+            } else {
+                alert('Seu navegador não suporta a API WebRTC.');
+            }
+        });
     </script>
 </body>
 </html>
