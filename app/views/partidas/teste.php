3820b6b11245f320783fd84b8fffc97839c8340f
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-14 23:01:00 -0300

    api camera

--- app/views/partidas/teste.php
+++ app/views/partidas/teste.php
@@ -4,33 +4,24 @@
     <title>Leitor de QR Code</title>
 </head>
 <body>
-    <video id="scanner" autoplay></video>
-    <button id="startCamera">Abrir Câmera</button>
+<script src="../../api/webcamjs/webcam.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/instascan/2.0.0/instascan.min.js"></script>
-    <script>
-        const scanner = new Instascan.Scanner({ video: document.getElementById('scanner') });
+<div id="my_camera" style="width:320px; height:240px;"></div>
+<div id="my_result"></div>
 
-        // Event listener para o botão "Abrir Câmera"
-        document.getElementById('startCamera').addEventListener('click', function() {
-            Instascan.Camera.getCameras()
-                .then(function(cameras) {
-                    if (cameras.length > 0) {
-                        scanner.start(cameras[0]); // Use a primeira câmera encontrada
-                    } else {
-                        alert('Nenhuma câmera encontrada.');
-                    }
-                })
-                .catch(function(error) {
-                    console.error('Erro ao acessar a câmera:', error);
-                });
-        });
+<script language="JavaScript">
+    Webcam.set('constraints',{
+        facingMode: "environment"
+    });
+    Webcam.attach( '#my_camera' );
 
-        // Event listener para a leitura de QR codes
-        scanner.addListener('scan', function(content) {
-            alert('QR Code detectado: ' + content);
-            window.location.href = content; // Redirecionar para o site do QR code
-        });
-    </script>
+    function take_snapshot() {
+        Webcam.snap( function(data_uri) {
+            document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
+        } );
+    }
+</script>
+
+<a href="javascript:void(take_snapshot())">Take Snapshot</a>
 </body>
 </html>
