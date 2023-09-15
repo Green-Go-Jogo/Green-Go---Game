2ff0ca8cd7167fecf769e4e6d504a1f1858b5da9
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-14 22:45:19 -0300

    Update teste.php

--- app/views/partidas/teste.php
+++ app/views/partidas/teste.php
@@ -10,6 +10,29 @@
     <button id="startButton">Abrir Câmera</button>
     <video id="video" style="display:none;"></video>
 
-    <script src="../js/camera.js"></script>
+    <script>
+        // Seleciona o botão e o elemento de vídeo
+        const startButton = document.getElementById('startButton');
+        const video = document.getElementById('video');
+
+        // Verifica se o navegador suporta a API getUserMedia
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            // Adiciona um evento de clique ao botão
+            startButton.addEventListener('click', function () {
+                // Solicita permissão para acessar a câmera
+                navigator.mediaDevices.getUserMedia({ video: true })
+                    .then(function (stream) {
+                        // Define o vídeo para exibir o feed da câmera
+                        video.srcObject = stream;
+                        video.play();
+                    })
+                    .catch(function (error) {
+                        console.error('Erro ao acessar a câmera: ', error);
+                    });
+            });
+        } else {
+            console.error('O navegador não suporta a API getUserMedia.');
+        }
+    </script>
 </body>
 </html>
