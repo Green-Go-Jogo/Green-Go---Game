80d5a34478da7a662a61791baf5cc233764d484e
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-14 23:13:06 -0300

    APi camera 2

--- app/views/partidas/teste.php
+++ app/views/partidas/teste.php
@@ -5,47 +5,32 @@
 </head>
 <body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jsqr@1.1.1/dist/jsQR.js"></script>
+<script src="html5-qrcode.min.js"></script>
 
-<div id="my_camera" style="width: 320px; height: 240px;"></div>
+<div style="width: 500px" id="reader"></div>
 <div id="my_result"></div>
 
 <script language="JavaScript">
-    Webcam.set('constraints', {
-        facingMode: "environment"
-    });
-    Webcam.attach('#my_camera');
+    function onScanSuccess(decodedText, decodedResult) {
+    // Handle on success condition with the decoded text or result.
+    console.log(`Scan result: ${decodedText}`, decodedResult);
+}
 
-    function checkForQRCode() {
-        Webcam.snap(function (data_uri) {
-            var img = new Image();
-            img.src = data_uri;
-            img.onload = function () {
-                var canvas = document.createElement('canvas');
-                canvas.width = img.width;
-                canvas.height = img.height;
-                var ctx = canvas.getContext('2d');
-                ctx.drawImage(img, 0, 0, img.width, img.height);
+    var html5QrcodeScanner = new Html5QrcodeScanner(
+	    "reader", { fps: 10, qrbox: 250 });
+    html5QrcodeScanner.render(onScanSuccess);
 
-                // Lê o QR code usando o jsQR
-                var code = jsQR(ctx.getImageData(0, 0, img.width, img.height), img.width, img.height);
-
-                if (code) {
-                    // Exibe o resultado do QR code
-                    document.getElementById('my_result').textContent = 'Conteúdo do QR code: ' + code.data;
-                } else {
-                    // Não foi encontrado um QR code
-                    document.getElementById('my_result').textContent = 'Aponte a câmera para um QR code.';
-                }
-
-                // Continua verificando em um loop
-                requestAnimationFrame(checkForQRCode);
-            };
-        });
-    }
-
-    // Iniciar a verificação ao carregar a página
-    checkForQRCode();
+    var html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", { fps: 10, qrbox: 250 });
+        
+function onScanSuccess(decodedText, decodedResult) {
+    // Handle on success condition with the decoded text or result.
+    console.log(`Scan result: ${decodedText}`, decodedResult);
+    // ...
+    html5QrcodeScanner.clear();
+    // ^ this will stop the scanner (video feed) and clear the scan area.
+}
+    
 </script>
 </body>
 </html>
