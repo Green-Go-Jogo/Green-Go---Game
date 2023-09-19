743e568e64e9fbd86f168e81c9c44ea9c4645b09
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-18 21:25:02 -0300

    Update teste.php

--- app/views/partidas/teste.php
+++ app/views/partidas/teste.php
@@ -1,100 +1,104 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>Instascan</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
-  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-</head>
-<body>
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#qrScannerModal">
-    Abrir Scanner
-  </button>
+  <head>
+    <title>Instascan</title>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    />
+    <script
+      type="text/javascript"
+      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"
+    ></script>
+  </head>
+  <body>
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target="#qrScannerModal"
+    >
+      Abrir Scanner
+    </button>
 
-  <div class="modal fade" id="qrScannerModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Scanner de QR Code</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close" onclick="stopScanner()">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <video id="preview" width="100%"></video>
-          <div id="cameraInfo"></div>
+    <div
+      class="modal fade"
+      id="qrScannerModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalCenterTitle"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">
+              Scanner de QR Code
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onclick="stopScanner()"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <video
+              id="preview"
+              width="100%"
+              style="height: 300px"
+            ></video>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <script>
-    let scanner = null;
+    <script>
+      let scanner = null;
 
-    function stopScanner() {
-      if (scanner) {
-        scanner.stop();
-      }
-    }
-
-    function startScanner() {
-      Instascan.Camera.getCameras().then(function (cameras) {
-        let cameraInfo = document.getElementById('cameraInfo');
-        cameraInfo.innerHTML = '';
-
-        if (cameras.length > 0) {
-          cameraInfo.innerHTML += '<p>Câmeras disponíveis:</p>';
-          cameras.forEach(function (camera, index) {
-            cameraInfo.innerHTML += '<p>Câmera ' + index + ': ' + camera.name + '</p>';
-          });
-
-          const videoElement = document.getElementById('preview');
-          videoElement.style.height = '';  // Limpa o estilo de altura
-
-          // Verifica a orientação da tela para decidir qual câmera abrir
-          const isLandscape = window.innerWidth > window.innerHeight;
-
-          // Escolhe a câmera com base no nome
-          const selectedCamera = cameras.find(camera => {
-            if (isLandscape) {
-              return camera.name.includes('environment');
-            } else {
-              return camera.name.includes('front');
-            }
-          });
-
-          // Inicia o scanner com a câmera selecionada
-          scanner.start(selectedCamera);
-        } else {
-          console.error('Não existe câmera no dispositivo!');
-          cameraInfo.innerText = 'Não há câmeras disponíveis no dispositivo.';
+      function stopScanner() {
+        if (scanner) {
+          scanner.stop();
         }
-      }).catch(function (e) {
-        console.error(e);
-      });
-    }
+      }
 
-    $('#qrScannerModal').on('hidden.bs.modal', function () {
-      stopScanner();
-    });
-
-    $('#qrScannerModal').on('shown.bs.modal', function () {
-      scanner = new Instascan.Scanner({
-        video: document.getElementById('preview'),
+      $('#qrScannerModal').on('hidden.bs.modal', function () {
+        stopScanner();
       });
 
-      scanner.addListener('scan', function (content) {
-        window.location.href = content;
-      });
+      $('#qrScannerModal').on('shown.bs.modal', function () {
+        scanner = new Instascan.Scanner({
+          video: document.getElementById('preview'),
+        });
 
-      startScanner();
-    });
-  </script>
-</body>
+        scanner.addListener('scan', function (content) {
+          alert('Escaneou o conteudo: ' + content);
+          window.open(content, '_blank');
+        });
+
+        Instascan.Camera.getCameras().then((cameras) => {
+          if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+          } else {
+            console.error('Não existe câmera no dispositivo!');
+          }
+        });
+      });
+    </script>
+  </body>
 </html>
