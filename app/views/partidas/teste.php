0ffa5b2c63622301b93c0463014350f43376d443
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-19 11:50:08 -0300

    Update teste.php

--- app/views/partidas/teste.php
+++ app/views/partidas/teste.php
@@ -1,104 +1,62 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Instascan</title>
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    />
-    <script
-      type="text/javascript"
-      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    ></script>
-    <script
-      type="text/javascript"
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-    ></script>
-    <script
-      type="text/javascript"
-      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-    ></script>
-    <script
-      type="text/javascript"
-      src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"
-    ></script>
-  </head>
-  <body>
-    <button
-      type="button"
-      class="btn btn-primary"
-      data-toggle="modal"
-      data-target="#qrScannerModal"
-    >
-      Abrir Scanner
-    </button>
-
-    <div
-      class="modal fade"
-      id="qrScannerModal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalCenterTitle"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLongTitle">
-              Scanner de QR Code
-            </h5>
-            <button
-              type="button"
-              class="close"
-              data-dismiss="modal"
-              aria-label="Close"
-              onclick="stopScanner()"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <video
-              id="preview"
-              width="100%"
-              style="height: 300px"
-            ></video>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <script>
-      let scanner = null;
-
-      function stopScanner() {
-        if (scanner) {
-          scanner.stop();
-        }
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Scanner de QR Code</title>
+</head>
+<body>
+  <h1>Scanner de QR Code</h1>
+  <video id="video" width="400" height="300" autoplay></video>
+  <script src="https://cdn.jsdelivr.net/npm/jsqr/dist/jsQR.min.js"></script>
+  <script>
+    async function startCamera() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+        const videoElement = document.getElementById('video');
+        videoElement.srcObject = stream;
+        videoElement.play();
+      } catch (error) {
+        console.error('Error accessing camera:', error);
       }
+    }
 
-      $('#qrScannerModal').on('hidden.bs.modal', function () {
-        stopScanner();
-      });
+    function scanQRCode(videoElement) {
+      const canvasElement = document.createElement('canvas');
+      const context = canvasElement.getContext('2d');
 
-      $('#qrScannerModal').on('shown.bs.modal', function () {
-        scanner = new Instascan.Scanner({
-          video: document.getElementById('preview'),
-        });
+      canvasElement.width = videoElement.videoWidth;
+      canvasElement.height = videoElement.videoHeight;
+      context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
 
-        scanner.addListener('scan', function (content) {
-          alert('Escaneou o conteudo: ' + content);
-          window.open(content, '_blank');
-        });
+      const imageData = context.getImageData(0, 0, canvasElement.width, canvasElement.height);
+      const code = jsQR(imageData.data, imageData.width, imageData.height);
 
-        Instascan.Camera.getCameras().then((cameras) => {
-          if (cameras.length > 0) {
-            scanner.start(cameras[0]);
-          } else {
-            console.error('Não existe câmera no dispositivo!');
-          }
-        });
-      });
-    </script>
-  </body>
+      if (code) {
+        console.log('QR Code scanned:', code.data);
+        alert('QR Code scanned: ' + code.data);
+      } else {
+        console.log('No QR code detected.');
+      }
+    }
+
+    function startQRScanning(videoElement) {
+      setInterval(() => {
+        scanQRCode(videoElement);
+      }, 1000);
+    }
+
+    async function initializeScanner() {
+      try {
+        await startCamera();
+        const videoElement = document.getElementById('video');
+        startQRScanning(videoElement);
+      } catch (error) {
+        console.error('Error initializing scanner:', error);
+      }
+    }
+
+    initializeScanner();
+  </script>
+</body>
 </html>
