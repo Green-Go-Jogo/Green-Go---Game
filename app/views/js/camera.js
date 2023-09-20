5dc084f853898b15edc91e564478308d37d2f1f4
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-20 00:27:53 -0300

    Update camera.js

--- app/views/js/camera.js
+++ app/views/js/camera.js
@@ -1,46 +1,48 @@
 let videoElement;
 
-    async function startCamera() {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
-        videoElement = document.getElementById('video');
-        videoElement.srcObject = stream;
-        videoElement.play();
-      } catch (error) {
-        console.error('Error accessing camera:', error);
-      }
-    }
-
-    function scanQRCode() {
-      const canvasElement = document.createElement('canvas');
-      const context = canvasElement.getContext('2d');
-
-      canvasElement.width = videoElement.videoWidth;
-      canvasElement.height = videoElement.videoHeight;
-      context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
-
-      const imageData = context.getImageData(0, 0, canvasElement.width, canvasElement.height);
-      const code = jsQR(imageData.data, imageData.width, imageData.height);
-
-      if (code) {
-        console.log('QR Code scanned:', code.data);
-        window.location.href = code.data;  // Redireciona para a URL lida no QR code
-        $('#qrScannerModal').modal('hide');  // Fecha o modal após o redirecionamento
-      }
-    }
-
-    // Inicializa o scanner quando o modal é mostrado
-    $('#qrScannerModal').on('shown.bs.modal', function () {
-      startCamera();
-      setInterval(scanQRCode, 1000);  // Escaneia a cada segundo
+async function startCamera() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: { exact: 'environment' } }  // Alteração para usar a câmera traseira
     });
+    videoElement = document.getElementById('video');
+    videoElement.srcObject = stream;
+    videoElement.play();
+  } catch (error) {
+    console.error('Error accessing camera:', error);
+  }
+}
 
-    // Limpa o stream da câmera quando o modal é fechado
-    $('#qrScannerModal').on('hidden.bs.modal', function () {
-      if (videoElement && videoElement.srcObject) {
-        const stream = videoElement.srcObject;
-        const tracks = stream.getTracks();
-        tracks.forEach(track => track.stop());
-        videoElement.srcObject = null;
-      }
-    });
+function scanQRCode() {
+  const canvasElement = document.createElement('canvas');
+  const context = canvasElement.getContext('2d');
+
+  canvasElement.width = videoElement.videoWidth;
+  canvasElement.height = videoElement.videoHeight;
+  context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+
+  const imageData = context.getImageData(0, 0, canvasElement.width, canvasElement.height);
+  const code = jsQR(imageData.data, imageData.width, imageData.height);
+
+  if (code) {
+    console.log('QR Code scanned:', code.data);
+    window.location.href = code.data;  // Redireciona para a URL lida no QR code
+    $('#qrScannerModal').modal('hide');  // Fecha o modal após o redirecionamento
+  }
+}
+
+// Inicializa o scanner quando o modal é mostrado
+$('#qrScannerModal').on('shown.bs.modal', function () {
+  startCamera();
+  setInterval(scanQRCode, 1000);  // Escaneia a cada segundo
+});
+
+// Limpa o stream da câmera quando o modal é fechado
+$('#qrScannerModal').on('hidden.bs.modal', function () {
+  if (videoElement && videoElement.srcObject) {
+    const stream = videoElement.srcObject;
+    const tracks = stream.getTracks();
+    tracks.forEach(track => track.stop());
+    videoElement.srcObject = null;
+  }
+});
