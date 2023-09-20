d3b01fff6dbdf1a1a1cdbcb14e79545573c3a77e
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-20 00:06:28 -0300

    Update camera.js

--- app/views/js/camera.js
+++ app/views/js/camera.js
@@ -1,14 +1,8 @@
 let videoElement;
 const constraints = {
   video: {
-    width: { min: 1280,
-      ideal: 1920,
-      max: 2560, },
-    height: { min: 720,
-      ideal: 1080,
-      max: 1440, },
     facingMode: "environment"
-  }
+  },
 };
     async function startCamera() {
       try {
