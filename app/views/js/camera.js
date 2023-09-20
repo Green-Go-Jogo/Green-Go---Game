00b8a01a5564c3d03e8b072be292de7e30736ac9
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-09-20 10:15:38 -0300

    CAMERA ULTIMATO

--- app/views/js/camera.js
+++ app/views/js/camera.js
@@ -1,48 +1,38 @@
-let videoElement;
 
-async function startCamera() {
-  try {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    const rearCamera = devices.find(device => device.kind === 'videoinput' && device.label.includes('rear'));
-    
-    if (rearCamera) {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: rearCamera.deviceId } });
-      videoElement = document.getElementById('video');
-      videoElement.srcObject = stream;
-      videoElement.play();
-      console.log('Rear camera started successfully.');
-    } else {
-      console.error('No rear camera found.');
-    }
-  } catch (error) {
-    console.error('Error accessing camera:', error);
-  }
-}
-    function scanQRCode() {
-      const canvasElement = document.createElement('canvas');
-      const context = canvasElement.getContext('2d');
-      canvasElement.width = videoElement.videoWidth;
-      canvasElement.height = videoElement.videoHeight;
-      context.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
-      const imageData = context.getImageData(0, 0, canvasElement.width, canvasElement.height);
-      const code = jsQR(imageData.data, imageData.width, imageData.height);
-      if (code) {
-        console.log('QR Code scanned:', code.data);
-        window.location.href = code.data;  // Redireciona para a URL lida no QR code
-        $('#qrScannerModal').modal('hide');  // Fecha o modal após o redirecionamento
+      let scanner = null;
+
+      function initScanner() {
+          scanner = new Html5QrcodeScanner('reader', {
+              qrbox: {
+                  width: 250,
+                  height: 250,
+              },
+              fps: 20,
+          });
       }
-    }
-    // Inicializa o scanner quando o modal é mostrado
-    $('#qrScannerModal').on('shown.bs.modal', function () {
-      startCamera();
-      setInterval(scanQRCode, 1000);  // Escaneia a cada segundo
-    });
-    // Limpa o stream da câmera quando o modal é fechado
-    $('#qrScannerModal').on('hidden.bs.modal', function () {
-      if (videoElement && videoElement.srcObject) {
-        const stream = videoElement.srcObject;
-        const tracks = stream.getTracks();
-        tracks.forEach(track => track.stop());
-        videoElement.srcObject = null;
+
+      function success(result) {
+          window.location.href = result;
+          scanner.clear();
       }
-    });
+
+      function error(err) {
+          console.error(err);
+      }
+
+      $('#qrScannerModal').on('shown.bs.modal', function () {
+          initScanner();
+          scanner.render(success, error);
+      });
+
+      function stopScanner() {
+          if (scanner) {
+              scanner.stop().then(ignore => {
+                  scanner.clear();
+                  scanner = null;
+                  $('#qrScannerModal').modal('hide');
+              }).catch(err => {
+                  console.error(err);
+              });
+          }
+      }
