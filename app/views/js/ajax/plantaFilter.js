861f00b918eccb0b9d41c8af9f2ad8592b89449b
BielMandelli <gabriel.mandellicardoso@gmail.com>
2023-11-10 11:23:38 -0300

    Front Filtro

--- app/views/js/ajax/plantaFilter.js
+++ app/views/js/ajax/plantaFilter.js
@@ -8,24 +8,24 @@ var caracteristicasSelected = [];
 var caracteristicas = [];
 
 var ADMs = [];
-var selectedValues = []; 
+var selectedValues = [];
 
 var zonas = [];
-var selectedZonas = []; 
+var selectedZonas = [];
 
 var busca = "";
 
 var cardNull = false;
 
 function openFiltros() {
-      var xhttp = new XMLHttpRequest();
+    var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "filtrarExec.php?function=getFiltros", true);
-  
-    xhttp.onreadystatechange = function() {
+
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var retorno = xhttp.responseText;
             var Array = JSON.parse(retorno);
-            
+
             ADMs = Array[0];
             caracteristicas = Array[1];
             caracteristicas = Object.entries(caracteristicas).slice(-1);
@@ -60,6 +60,7 @@ function show() {
 
 function createFiltroCaracteristicas() {
     i = 0;
+    var quebraLinhaApos = window.innerWidth >= 700 ? 5 : 2; // Troque x pelo tamanho desejado
     caracteristicas[0][1].forEach(element => {
         input = document.createElement('input');
         input.type = "checkbox";
@@ -67,30 +68,41 @@ function createFiltroCaracteristicas() {
         input.id = "filtroCheckbox";
         input.className = "filtroKids filtroKids" + element;
         input.value = element;
-        input.setAttribute("oninput", "saveCaracteristicas('"+element+"')");
+        input.setAttribute("oninput", "saveCaracteristicas('" + element + "')");
 
-        if(caracteristicasSelected.includes(element)) {
+        if (caracteristicasSelected.includes(element)) {
             input.checked = true;
         }
         label = document.createElement('label');
         label.for = element;
         label.innerHTML = element;
         label.className = "filtroKids";
-        br = document.createElement('br');
-        br.className = "filtroKids";
 
         pai.appendChild(input);
         pai.appendChild(label);
-        pai.appendChild(br);
+
+        if (i > 0 && i % quebraLinhaApos === quebraLinhaApos - 1) {
+            br = document.createElement('br');
+            br.className = "filtroKids";
+            pai.appendChild(br);
+        }
         i++;
     });
 }
 
 function createFiltroADMs() {
+    labelAdm = document.createElement('label');
+    labelAdm.htmlFor = "selectAdm";
+    labelAdm.innerHTML = "Escolher Administrador:";
+    labelAdm.className = "labelAdm filtroKids";
+    br1 = document.createElement('br');
+    br1.className = 'filtroKids'; 
     select = document.createElement('select');
     select.className = "filtroKids";
     select.id = "selectAdm";
     // select.setAttribute("oninput", "findPlantas()");
+    pai.appendChild(labelAdm);
+    pai.appendChild(br1);
     pai.appendChild(select);
 
     ADMs.forEach(element => {
@@ -104,21 +116,21 @@ function createFiltroADMs() {
 
         select.appendChild(option);
     });
-    
+
     const sendFiltroButton = document.createElement("button");
     sendFiltroButton.className = "btn filtroKids";
     sendFiltroButton.setAttribute('id', 'filtroAdmButton');
-    sendFiltroButton.innerHTML = "Adicionar Administrador";
+    sendFiltroButton.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='#20A494' viewBox='0 0 16 16'><path d='M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z'/></svg>";
     sendFiltroButton.addEventListener("click", addSelectedValue);  // Adicionando um evento de clique
     pai.appendChild(sendFiltroButton);
 
-    if(selectedValues.length > 0) {
-        displaySelectedValues();   
+    if (selectedValues.length > 0) {
+        displaySelectedValues();
     }
-    
+
     displayDiv.id = 'displaySelectedValues';  // Atribuindo um ID para a área de exibição
     pai.appendChild(displayDiv);
- 
+
 }
 function addSelectedValue() {
     const selectedOption = select.options[select.selectedIndex];
@@ -134,7 +146,7 @@ function addSelectedValue() {
 }
 function displaySelectedValues() {
     displayDiv.innerHTML = selectedValues.map(value => {
-        return `<div class='adms'>${value} <button class='btn' onclick="removeSelectedValue('${value}')">Remover</button></div>`;
+        return `<div class='adms'>${value} <button class='btn' onclick="removeSelectedValue('${value}')"><a><i class="fas fa-trash" style="color: #078071;"></i></a></button></div>`;
     }).join('');
 }
 function removeSelectedValue(value) {
@@ -147,13 +159,21 @@ function removeSelectedValue(value) {
 }
 
 function createFiltroZona() {
+    labelZonas = document.createElement('label');
+    labelZonas.htmlFor = "selectZona";
+    labelZonas.innerHTML = "Escolher Zona:";
+    labelZonas.className = "labelZona filtroKids";
+    br1 = document.createElement('br');
+    br1.className = 'filtroKids'; 
     selectZonas = document.createElement('select');
     selectZonas.className = "filtroKids";
     selectZonas.id = "selectZona";
-    // select.setAttribute("oninput", "findPlantas()");
+
+    pai.appendChild(labelZonas);
+    pai.appendChild(br1);
     pai.appendChild(selectZonas);
     zonas.forEach(element => {
-    
+
         option = document.createElement('option');
         option.name = element["NomeZona"];
         option.className = "filtroKids";
@@ -162,21 +182,21 @@ function createFiltroZona() {
 
         selectZonas.appendChild(option);
     });
-    
+
     const sendZonaButton = document.createElement("button");
     sendZonaButton.className = "btn filtroKids";
     sendZonaButton.setAttribute('id', 'filtroZonaButton');
-    sendZonaButton.innerHTML = "Adicionar Zona";
+    sendZonaButton.innerHTML = "<svg xmlns='http://www.w3.org/2000/svg' width='30' height='20' fill='#20A494' viewBox='0 0 16 16'><path d='M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z'/></svg>";
     sendZonaButton.addEventListener("click", addSelectedZona);  // Adicionando um evento de clique
     pai.appendChild(sendZonaButton);
 
-    if(selectedZonas.length > 0) {
-        displaySelectedZonas();   
+    if (selectedZonas.length > 0) {
+        displaySelectedZonas();
     }
-    
+
     displayDivZona.id = 'displaySelectedZonas';  // Atribuindo um ID para a área de exibição
     pai.appendChild(displayDivZona);
- 
+
 }
 function addSelectedZona() {
     const selectedOptionZonas = selectZonas.options[selectZonas.selectedIndex];
@@ -192,7 +212,7 @@ function addSelectedZona() {
 }
 function displaySelectedZonas() {
     displayDivZona.innerHTML = selectedZonas.map(valueZonas => {
-        return `<div class='zonas'>${valueZonas} <button class='btn' onclick="removeSelectedZona('${valueZonas}')">Remover</button></div>`;
+        return `<div class='zonas'>${valueZonas} <button class='btn' onclick="removeSelectedZona('${valueZonas}')"><a><i class="fas fa-trash" style="color: #078071;"></i></a></button></div>`;
     }).join('');
 }
 function removeSelectedZona(valueZonas) {
@@ -207,14 +227,14 @@ function removeSelectedZona(valueZonas) {
 function createStopFiltroButton() {
     button = document.createElement('button');
     button.setAttribute("onclick", "stopFiltros()");
-    button.className = "btn filtroKids";
+    button.className = "btn filtroKids limpar";
     button.innerHTML = "Limpar Filtro";
     pai.appendChild(button);
 
 }
 function stopFiltros() {
-    removeChildren({parentId:'displaySelectedValues',childName:'adms'});
-    removeChildren({parentId:'displaySelectedZonas',childName:'zonas'});
+    removeChildren({ parentId: 'displaySelectedValues', childName: 'adms' });
+    removeChildren({ parentId: 'displaySelectedZonas', childName: 'zonas' });
     document.getElementById("buscar").value = "";
     document.querySelectorAll('input[type=checkbox]').forEach(el => el.checked = false);
     selectedValues = [];
@@ -225,14 +245,14 @@ function stopFiltros() {
 }
 
 function closeFiltros() {
-    removeChildren({parentId:'filtroParent',childName:'filtroKids'});
-    removeChildren({parentId:'displaySelectedValues',childName:'adms'});
-    removeChildren({parentId:'displaySelectedZonas',childName:'zonas'});
+    removeChildren({ parentId: 'filtroParent', childName: 'filtroKids' });
+    removeChildren({ parentId: 'displaySelectedValues', childName: 'adms' });
+    removeChildren({ parentId: 'displaySelectedZonas', childName: 'zonas' });
 
     FiltrosButton.innerHTML = "Abrir Filtro";
     FiltrosButton.setAttribute("onclick", "show()");
-   
-} 
+
+}
 
 
 function removeChildren(params) {
@@ -240,13 +260,13 @@ function removeChildren(params) {
     var childName = params.childName;
 
     var childNodesToRemove = document.getElementById(parentId).getElementsByClassName(childName);
-    for(var i=childNodesToRemove.length-1;i >= 0;i--){
+    for (var i = childNodesToRemove.length - 1; i >= 0; i--) {
         var childNode = childNodesToRemove[i];
         childNode.parentNode.removeChild(childNode);
     }
 }
 function saveCaracteristicas(caracteristica) {
-    if(caracteristicasSelected.includes(caracteristica) == true) {
+    if (caracteristicasSelected.includes(caracteristica) == true) {
         index = caracteristicasSelected.indexOf(caracteristica);
         caracteristicasSelected.splice(index, 1);
     }
@@ -260,30 +280,30 @@ function saveBusca() {
     findPlantas();
 }
 
-function findPlantas () {
+function findPlantas() {
     filtroArray = [caracteristicasSelected, busca, selectedValues, selectedZonas];
     var jsonString = JSON.stringify(filtroArray);
     $.ajax({
-         type: "POST",
-         url: "filtrarExec.php?function=findPlantas",
-         data: {data : jsonString},
-         cache: false,
- 
-         success: function(data){
+        type: "POST",
+        url: "filtrarExec.php?function=findPlantas",
+        data: { data: jsonString },
+        cache: false,
+
+        success: function (data) {
             var plantas = $.parseJSON(data);
-            removeChildren({parentId:'pai',childName:'card-kid'});
+            removeChildren({ parentId: 'pai', childName: 'card-kid' });
             createCards(plantas);
-         }
-     });
+        }
+    });
 }
 
 function createCards(plantas) {
-    
 
-    if(plantas.length == 0) {
-        if(cardNull == false) {
+
+    if (plantas.length == 0) {
+        if (cardNull == false) {
             div = document.createElement('div');
-            div.id ="msgNull";
+            div.id = "msgNull";
             div.innerHTML = "Nenhuma planta encontrada!";
             document.getElementById("pai").appendChild(div);
             cardNull = true;
@@ -293,16 +313,16 @@ function createCards(plantas) {
             return;
         }
     }
-    else { 
+    else {
         cardNull = false;
-        if(document.getElementById("msgNull") != undefined) {
+        if (document.getElementById("msgNull") != undefined) {
             document.getElementById("pai").removeChild(document.getElementById("msgNull"));
         }
-        
+
     }
 
     plantas.forEach(element => {
-    console.log(element);
+        console.log(element);
         div = document.createElement('div');
         div.className = "card-kid col-md-4";
         document.getElementById("pai").appendChild(div);
@@ -316,7 +336,7 @@ function createCards(plantas) {
         div.appendChild(divCard);
 
         a1 = document.createElement('a');
-        a1.href = "visualizarPlanta.php?idp="+element['IdPlanta']+"&ide="+element['Especie']['IdEspecie'];
+        a1.href = "visualizarPlanta.php?idp=" + element['IdPlanta'] + "&ide=" + element['Especie']['IdEspecie'];
 
         img = document.createElement('img');
         img.src = element['ImagemPlanta'];
@@ -330,8 +350,8 @@ function createCards(plantas) {
         divBody.className = "card-body";
 
         h5 = document.createElement('h5');
-        
-        if(element['NomeSocial'] != "") {
+
+        if (element['NomeSocial'] != "") {
             h5.id = "nomePlanta";
             h5.innerHTML = element["NomeSocial"];
         }
@@ -348,8 +368,8 @@ function createCards(plantas) {
 
         p1 = document.createElement('p');
         p1.className = 'card-text nome-texto';
-        
-        p1.innerHTML = "<a id='codplanta'>Código: "+element['CodNumerico']+"<br><br></a>Pontuação: "+element['Pontos']+"<br>";
+
+        p1.innerHTML = "<a id='codplanta'>Código: " + element['CodNumerico'] + "<br><br></a>Pontuação: " + element['Pontos'] + "<br>";
         divBody.appendChild(p1);
 
         p2 = document.createElement('p');
@@ -364,7 +384,7 @@ function createCards(plantas) {
         p3.innerHTML = element['Usuario']['nomeUsuario'];
         divBody.appendChild(p3);
 
-        
+
         // Crie o elemento button impressao
         btn = document.createElement('button');
         btn.setAttribute('type', 'button');
@@ -381,21 +401,21 @@ function createCards(plantas) {
         divBody.appendChild(br69);
 
         if ((tipoUsuarioLogado == 3 && nomeUsuarioLogado == element['Usuario']['nomeUsuario']) || tipoUsuarioLogado == 2) {
-        a2 = document.createElement('a');
-        a2.href = "editarPlanta.php?id="+element['IdPlanta'];
-        a2.id = 'editas';
-        a2.innerHTML = "Editar";
-        divBody.appendChild(a2);
+            a2 = document.createElement('a');
+            a2.href = "editarPlanta.php?id=" + element['IdPlanta'];
+            a2.id = 'editas';
+            a2.innerHTML = "Editar";
+            divBody.appendChild(a2);
         }
 
         if ((tipoUsuarioLogado == 3 && nomeUsuarioLogado == element['Usuario']['nomeUsuario']) || tipoUsuarioLogado == 2) {
-        a3 = document.createElement('a');
-        a3.href = "deletarPlanta.php?id="+element['IdPlanta'];
-        a3.setAttribute('onclick', 'return confirm(\"Confirma a exclusão da Planta?\");');
-        a3.id = 'excluas'
-        a3.className = 'btn btn-alert excluir';
-        a3.innerHTML = "Excluir";
-        divBody.appendChild(a3);
+            a3 = document.createElement('a');
+            a3.href = "deletarPlanta.php?id=" + element['IdPlanta'];
+            a3.setAttribute('onclick', 'return confirm(\"Confirma a exclusão da Planta?\");');
+            a3.id = 'excluas'
+            a3.className = 'btn btn-alert excluir';
+            a3.innerHTML = "Excluir";
+            divBody.appendChild(a3);
         }
 
         br3 = document.createElement('br');
